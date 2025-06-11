628372e06590614528042d921f5f1521e22005c1
Nguyễn Thành Đạt <datnguyen9240@gmail.com>
2025-06-11 10:17:31 +0700

    fix: link

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
-import ChatWidget from "@/components/ChatWitget";
+import ChatWidget from '@/components/ChatWitget';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
@@ -7,24 +8,23 @@ export default function Home() {
         Chào mừng đến với EduConnect
       </h1>
       <p className="text-lg text-gray-700 text-center max-w-xl mb-6">
-        Nền tảng kết nối số thông minh giữa <strong>phụ huynh</strong>,{" "}
-        <strong>giáo viên</strong>,<strong> học sinh</strong> và{" "}
-        <strong>ban giám hiệu</strong>, giúp theo dõi và nâng cao chất lượng
-        giáo dục trung học.
+        Nền tảng kết nối số thông minh giữa <strong>phụ huynh</strong>, <strong>giáo viên</strong>,
+        <strong> học sinh</strong> và <strong>ban giám hiệu</strong>, giúp theo dõi và nâng cao chất
+        lượng giáo dục trung học.
       </p>
       <div className="flex flex-col sm:flex-row gap-4 mt-6">
-        <a
-          href="/login"
+        <Link
+          to="/login"
           className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow hover:bg-blue-700 transition"
         >
           Đăng nhập
-        </a>
-        <a
-          href="/register"
+        </Link>
+        <Link
+          to="/register"
           className="px-6 py-3 border border-blue-600 text-blue-600 font-semibold rounded-lg hover:bg-blue-50 transition"
         >
           Đăng ký tài khoản
-        </a>
+        </Link>
       </div>
       <div className="mt-12 text-sm text-gray-500">
         © 2025 FPT EduConnect – Powered by Group SWD392
